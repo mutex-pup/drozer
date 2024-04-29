496ea63625b37671b4455a24693bdeec171e94aa
cyberMilosz <milosz.gaczkowski@withsecure.com>
2024-04-29 19:38:07 +0100

    Update URLs

--- src/WithSecure/common/cmd_ext.py
+++ src/WithSecure/common/cmd_ext.py
@@ -279,9 +279,9 @@ class Cmd(cmd.Cmd):
             latest = meta.latest_version()
             if latest is not None:
                 if meta.version > latest:
-                    print("It seems that you are running a drozer pre-release. Brilliant!\n\nPlease send any bugs, feature requests or other feedback to our Github project:\nhttp://github.com/mwrlabs/drozer.\n\nYour contributions help us to make drozer awesome.\n")
+                    print("It seems that you are running a drozer pre-release. Brilliant!\n\nPlease send any bugs, feature requests or other feedback to our GitHub project:\nhttps://github.com/WithSecureLabs/drozer\n\nYour contributions help us to make drozer awesome.\n")
                 elif meta.version < latest:
-                    print("It seems that you are running an old version of drozer. drozer v%s was\nreleased on %s. We suggest that you update your copy to make sure that\nyou have the latest features and fixes.\n\nTo download the latest drozer visit: https://labs.withsecure.com/tools/drozer/\n" % (latest, latest.date))
+                    print("It seems that you are running an old version of drozer. drozer v%s was\nreleased on %s. We suggest that you update your copy to make sure that\nyou have the latest features and fixes.\n\nTo download the latest drozer visit:\nhttps://github.com/WithSecureLabs/drozer/releases\n" % (latest, latest.date))
         except Exception as e:
             #silence this exception unless in debug mode
             self.handleException(e, shutup=True)
